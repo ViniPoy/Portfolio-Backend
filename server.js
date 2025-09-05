818e195639f50b42960866ae84af9bce155380ee
ViniPoy <vinipoy@gmail.com>
2025-09-05 14:07:59 +0200

    correction numéro 2 du fichier server.js

--- server.js
+++ server.js
@@ -3,19 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = require('./app');
 
-const normalizePort = val => {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
-};
-
-const port = normalizePort(process.env.PORT || 4000)
-app.set('port', port);
+const port = process.env.PORT || 4000;
 
 const errorHandeler = error => {
     if (error.syscall !== 'listen') {
