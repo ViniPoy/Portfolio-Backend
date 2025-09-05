ff0bb47c91b442c6b83122989de105e9790f0ac3
ViniPoy <vinipoy@gmail.com>
2025-09-05 15:05:45 +0200

    modifs des CORS pour permettre à la version en ligne de fonctionner

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const allowedOrigins = [
     "http://localhost:4000",
     "http://localhost:3000",
     "http://127.0.0.1:5500",
-    "https://vinipoy.github.io/Portfolio/"
+    "https://vinipoy.github.io"
 ];
 
 app.use(cors({
