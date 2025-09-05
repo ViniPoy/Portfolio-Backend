0588021b7f5b2f1d1bdfe0fef735ad29976b4479
ViniPoy <vinipoy@gmail.com>
2025-09-05 16:15:56 +0200

    remise en état pour test CORS sur render

--- app.js
+++ app.js
@@ -21,20 +21,16 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: "*"
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Origin", "X-Requested-With", "Content", "Accept", "Content-Type", "Authorization"]
 }));
-
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error("Not allowed by CORS"));
-//         }
-//     },
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-//     allowedHeaders: ["Origin", "X-Requested-With", "Content", "Accept", "Content-Type", "Authorization"]
-// }));
 
 app.use(express.json());
 
