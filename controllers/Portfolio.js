dedef7dcdce2fe8bf7f4cb8d3acac9223f512e09
ViniPoy <vinipoy@gmail.com>
2025-09-09 12:52:09 +0200

    modif des controleur pour effacer les images de la BDD en même temps que la suppression de projet ou que la modification de projets (pas encore implémenter dans le front)

--- controllers/Portfolio.js
+++ controllers/Portfolio.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const Portfolio = require ("../models/Portfolio");
 
 exports.getAllProjects = (req, res, next) => {
@@ -37,13 +39,47 @@ exports.createProject = (req, res, next) => {
 };
 
 exports.updateProject = (req, res, next) => {
-    Portfolio.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        .then(project => res.status(200).json(project))
-        .catch(error => res.status(400).json({ error }));
+    Portfolio.findById(req.params.id)
+        .then(project => {
+            if (!project) {
+                return res.status(404).json({ error: "Projet introuvable !" });
+            }
+            const data = { ...req.body };
+            if (data.skills) data.skills = JSON.parse(data.skills);
+            if (data.link) data.link = JSON.parse(data.link);
+            if(req.file) {
+                if (project.imageUrl) {
+                    const oldFileName = project.imageUrl.split("/images/")[1];
+                    const oldFilePath = path.join(__dirname, "../images/", oldFileName);
+                    fs.unlink(oldFilePath, err => {
+                        if (err) console.error("Erreur lors de la suppression de l'ancienne image :", err);
+                    });
+                }
+                data.imageUrl = `${req.protocol}://${req.get("host")}/images/${req.file.filename}`;
+            }
+            Portfolio.findByIdAndUpdate(req.params.id, data, { new: true })
+                .then(project => res.status(200).json(project))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 exports.deleteProject = (req, res, next) => {
-    Portfolio.findByIdAndDelete(req.params.id)
-        .then(() => res.status(200).json({ message: "Projet supprimé !" }))
-        .catch(error => res.status(400).json({ error }));
+    Portfolio.findById(req.params.id)
+        .then(project => {
+            if (!project) {
+                return res.status(404).json({ error: "Projet introuvable !" })
+            }
+            const filename = project.imageUrl.split("/images/")[1];
+            const filePath = path.join(__dirname, "../images/", filename);
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.error("Erreur lors de la suppression du fichier :", err);
+                }
+                Portfolio.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: "Projet supprimé avec succès !" }))
+                    .catch(error => res.status(400).json({ error }));
+            })
+        })
+        .catch(error => res.status(500).json({ error }));
 };
