b899193d398e22455f3f208748a7d79a5371f314
ViniPoy <vinipoy@gmail.com>
2025-09-22 17:21:30 +0200

    ajout d'une modif pour la stabilité du backend.

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const adminRoutes = require("./routes/Admin");
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}) 
     .then(() => console.log('Connexion à mongoDB réussie !'))
     .catch(error => console.error('Connexion à mongoDB échouée :', error));
 
