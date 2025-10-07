c8529670c0237a356ce80c6136fce866860fc213
ViniPoy <vinipoy@gmail.com>
2025-10-07 18:00:14 +0200

    Remise de mutler comme il était

--- middleware/Multer.js
+++ middleware/Multer.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => callback(null, "backend-images"),
+    destination: (req, file, callback) => callback(null, "images"),
     filename: (req, file, callback) => callback(null, Date.now() + "-" + file.originalname)
 });
 
