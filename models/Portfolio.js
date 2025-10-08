c644c8d69f60bd40254bd5de5ac58e52b4c49156
ViniPoy <vinipoy@gmail.com>
2025-10-08 11:48:04 +0200

    Modification du schema pour les cartes du portfolio qui ne prenait pas les alt

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const portfolioSchema = new mongoose.Schema({
     imageUrl: {type: String, required: true},
+    alt: {type: String, required: true},
     title: {type: String, required: true},
     description: {type: String, required: true},
     skills: {type: [String], required: true},
